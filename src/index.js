d5aea18a11e0a7b2a8f14c6fd44d7160a21a0d14
Ilja M <i.meleshekvich@gmail.com>
2020-07-25 20:58:55 +0200

    adding useContext() react hooks

--- src/index.js
+++ src/index.js
@@ -1,46 +1,22 @@
-import React, {useState} from 'react';
+import React, {useContext} from 'react';
 import ReactDOM from 'react-dom';
- 
+
+const MyContext = React.createContext();
+
 const App = () => {
-  return (
-    <div>
-      <HookSwitcher />
-    </div>
-  );
+    return (
+        <MyContext.Provider value= 'Hello, World 123!'>
+            <Child />
+        </MyContext.Provider>
+    );
 };
- 
 
-const HookSwitcher = () => {
-  const [color, setColor] = useState('white');
-  const [fontSize, setFontSize] = useState(14);
-
-  return (
-    <div style= {{padding: '10px', 
-    backgroundColor: color,
-    fontSize: `${fontSize}px`}} >
-      Hello, World!
-      <br></br>
-      <button 
-        onClick= {()=> setColor('grey')}>
-        Dark
-      </button>
-      <button 
-        onClick= {() => setColor('white')}>
-        Light
-      </button>
-      <button onClick= {() =>  setFontSize((s) => s+2)}>
-        Font Size +
-      </button>
-
-    </div>
-  );
+const Child = () => {
+    const value = useContext(MyContext);
+    return <div>{value}</div>; 
 };
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
- 
+    <App />,
+    document.getElementById('root')
+  );
