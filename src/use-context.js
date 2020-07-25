3950a9238d077e97717cc48ec89d66e190980dce
Ilja M <i.meleshekvich@gmail.com>
2020-07-25 22:03:44 +0200

    adding useEffect() hook examples

---
+++ src/use-context.js
@@ -0,0 +1,22 @@
+import React, {useContext} from 'react';
+import ReactDOM from 'react-dom';
+
+const MyContext = React.createContext();
+
+const App = () => {
+    return (
+        <MyContext.Provider value= 'Hello, World 123!'>
+            <Child />
+        </MyContext.Provider>
+    );
+};
+
+const Child = () => {
+    const value = useContext(MyContext);
+    return <div>{value}</div>; 
+};
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
---
+++ src/use-effect.js
@@ -0,0 +1,5 @@
+const { useEffect } = require("react");
+
+useEffect(() => console.log('mount'), []);
+useEffect(() => console.log('update'));
+useEffect(() => () => console.log('unmount'), []);
